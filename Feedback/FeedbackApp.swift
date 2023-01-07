a1cb3b8dd3c286245835665a3a9e74d969ae406a
Jack Finnis <jack.finnis@icloud.com>
2023-01-07 08:07:45 +0000

    Initial Commit

---
+++ Feedback/FeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackApp.swift
+//  Feedback
+//
+//  Created by Jack Finnis on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
